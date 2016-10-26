a4efea3ebc4630fb90421d68df5efb241bf51517
FanWP <1215630349@qq.com>
2016-10-26 16:07:49 +0800

    初始化

---
+++ TianlangStar/TianlangStar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TianlangStar
+//
+//  Created by youyousiji on 16/10/26.
+//  Copyright © 2016年 yysj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
