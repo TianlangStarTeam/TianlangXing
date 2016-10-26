daea49aeb9886c84fdd561b82478a54671da046f
FanWP <1215630349@qq.com>
2016-10-26 16:24:56 +0800

    1.11

--- TianlangStar/TianlangStar/Project/Mine/Controller/MineVC.m
+++ TianlangStar/TianlangStar/Project/Mine/Controller/MineVC.m
@@ -10,28 +10,23 @@
 
 @interface MineVC ()
 
+
+
 @end
 
 @implementation MineVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 90, 30)];
+    [self.view addSubview:button];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
