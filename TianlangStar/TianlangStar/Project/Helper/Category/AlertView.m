5dc9eb0bff96a08bfa7af2f7d6af286592956691
FanWP <1215630349@qq.com>
2016-10-27 09:10:48 +0800

    FWP10-27

--- TianlangStar/TianlangStar/Project/Helper/Category/AlertView.m
+++ TianlangStar/TianlangStar/Project/Helper/Category/AlertView.m
@@ -22,17 +22,39 @@
     
     UIAlertAction *cancleAction = [UIAlertAction actionWithTitle:@"取消" style:(UIAlertActionStyleCancel) handler:nil];
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action)
-                               {
-                                   
-                                   LoginVC *loginVC = [[LoginVC alloc] init];
-                                   loginVC.view.y = 64;
-                                   [root.navigationController pushViewController:loginVC animated:YES];
-                               }];
+       {
+           
+           LoginVC *loginVC = [[LoginVC alloc] init];
+           loginVC.view.y = 64;
+           [root.navigationController pushViewController:loginVC animated:YES];
+       }];
     
     [alert addAction:cancleAction];
     [alert addAction:okAction];
     
     [root presentViewController:alert animated:YES completion:nil];
+}
+
+
+
+/** 自动登录 */
+- (void)loginUpdataSession
+{
+
+    YYLog(@"自动登录");
+}
+
+
+
+/**
+ *  @param message 提示信息
+ *  @param title   标题
+ */
+-(void)addAlertMessage:(NSString *)message title:(NSString *)title
+{
+
+    NSLog(@"%@",title);
+
 }
 
 
